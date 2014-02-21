7a1e54f7acc7c7529ba38ed418421b07abb5f1b3
John_and_Tony <tonyta.tt@gmail.com>
2014-02-20 20:26:24 -0600

    initial commit

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,10 +1,19 @@
 class BoggleBoard
+  attr_accessor :board
+
+  ALPHA = ("A".."Z").to_a
+
   def initialize
+    @board = Array.new(4) {Array.new(4, '_')}
   end
 
   def shake!
+    self.board = new_board
   end
 
+  def new_board
+    ALPHA.sample(16).each_slice(4).to_a
+  end
   # Defining to_s on an object controls how the object is
   # represented as a string, e.g., when you pass it to puts
   #
@@ -15,6 +24,17 @@ class BoggleBoard
   # board.shake!
   # puts board
   def to_s
-    "omg what is this? try printing me."
+    str = ''
+    board.each {|row| str << "#{row.join(' ')}\n" }
+    str << "\n"
   end
 end
+
+board = BoggleBoard.new
+puts board
+board.shake!
+puts board
+board.shake!
+puts board
+board.shake!
+puts board
